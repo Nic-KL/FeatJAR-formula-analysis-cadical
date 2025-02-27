053978f54f6269e929bdc4057c60fa5ffa08cecc
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2025-02-27 17:24:52 +0100

    Updates tests

--- src/test/java/de/featjar/analysis/cadical/solver/CadiCalSolverTest.java
+++ src/test/java/de/featjar/analysis/cadical/solver/CadiCalSolverTest.java
@@ -24,6 +24,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import de.featjar.Common;
 import de.featjar.analysis.cadical.computation.ComputeGetSolutionCadiCal;
+import de.featjar.base.FeatJAR;
 import de.featjar.base.computation.Computations;
 import de.featjar.base.data.Result;
 import de.featjar.formula.assignment.BooleanSolution;
@@ -37,9 +38,21 @@ import de.featjar.formula.structure.connective.BiImplies;
 import de.featjar.formula.structure.connective.Implies;
 import de.featjar.formula.structure.connective.Or;
 import de.featjar.formula.structure.predicate.Literal;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class CadiCalSolverTest extends Common {
+
+    @BeforeAll
+    public static void begin() {
+        FeatJAR.testConfiguration().initialize();
+    }
+
+    @AfterAll
+    public static void end() {
+        FeatJAR.deinitialize();
+    }
 
     @Test
     public void formulaHasSolution() {
